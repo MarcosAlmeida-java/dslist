23d548f27bc654076c0f3e3471bafabbc9537627
MarcosAlmeida-java <contato.marcosalmeida2006@gmail.com>
2025-05-20 16:24:32 -0300

    Projet created

---
+++ src/test/java/com/javaProjet/dslistt/DslisttApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaProjet.dslistt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslisttApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
